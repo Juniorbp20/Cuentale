e275e7749d2603ba691900f63a4c77e2c2bc7042
Juniorbp20 <Jbonifacio0707@gmail.com>
2026-03-05 21:49:37 -0400

    Sistema base del restaurante

---
+++ Form1.cs
@@ -0,0 +1,40 @@
+namespace Cuentale
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label22_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label14_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label19_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label11_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
