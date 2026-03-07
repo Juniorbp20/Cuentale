af9961f5af554f8feadb5e9b727ec7bed75ffbfe
Juniorbp20 <Jbonifacio0707@gmail.com>
2026-03-06 21:36:40 -0400

    Creer las clases modelos necesarias para el DbContext y cambie algunos label a toolStrip debajo del menuStrip superior

--- VentanaPrincipal.cs
+++ VentanaPrincipal.cs
@@ -25,6 +25,7 @@ namespace Cuentale
         private void label14_Click(object sender, EventArgs e)
         {
 
+
         }
 
         private void label19_Click(object sender, EventArgs e)
@@ -49,10 +50,55 @@ namespace Cuentale
 
         private void label15_Click(object sender, EventArgs e)
         {
-
+            MessageBox.Show("¡Hola! Soy Cuentale, tu asistente de IA para la gestión de proyectos.", "Bienvenido a Cuentale", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
 
         private void label16_Click(object sender, EventArgs e)
+        {
+            Application.Exit(); // Cierra la aplicación
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void VentanaPrincipal_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+
+        }
+
+        private void splitContainer1_Panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button9_Click(object sender, EventArgs e)
         {
             Application.Exit(); // Cierra la aplicación
         }
